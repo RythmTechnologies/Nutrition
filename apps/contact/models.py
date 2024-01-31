b94f20b4c4735025f623421ec074e03321aae25b
mciray <melihciray@gmail.com>
2024-02-01 02:47:40 +0300

    some changes

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Ad")
+    email = models.EmailField(verbose_name="E-posta")
+    message = models.TextField(verbose_name="Mesaj")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Gönderilme Tarihi")
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
