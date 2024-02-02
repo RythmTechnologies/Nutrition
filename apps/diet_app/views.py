942ba07b1ae4ab7dcea123710ea6894a864b4641
mciray <melihciray@gmail.com>
2024-02-03 00:58:08 +0300

    add index blog

--- apps/diet_app/views.py
+++ apps/diet_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from .models import HomeSlider,TopContent
+from apps.blog.models import Blog
 # Create your views here.
 def index(request):
     context={}
 
     sliders = HomeSlider.objects.all()
     Topcontent = TopContent.objects.all()
-
+    blogs = Blog.objects.all().order_by('-created_at')[:8]
     context["sliders"]=sliders
     context["Topcontent"]=Topcontent
+    context['blogs'] = blogs
     
 
     return render(request, 'index.html',context)
