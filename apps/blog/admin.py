53e6e9f6d0e48ad122833de189e7f257db5e7099
mciray <melihciray@gmail.com>
2024-02-03 19:33:27 +0300

    pagination,blog page added

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import Category, Label, Blog
+from .models import Category, Label, Blog,BlogAbout,BlogContent
 
+
+class BlogContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_blog_title',)
+
+    def get_blog_title(self, obj):
+        return obj.blog.title
+    get_blog_title.short_description = 'Blog Adı' 
+
+    
 
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(Category)
-admin.site.register(Label)
+admin.site.register(Label) 
+admin.site.register(BlogAbout)
+admin.site.register(BlogContent, BlogContentAdmin)
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,20 +1,47 @@
-from django.shortcuts import render, redirect
-from apps.diet_app.mixin import HttpRequest, HttpResponse, RedirectOrResponse
-from .models import Blog
-
+from django.shortcuts import render
+from apps.diet_app.mixin import HttpRequest, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Blog,BlogAbout,BlogContent
+import random
 
 def blog_view(request: HttpRequest) -> HttpResponse:
-  context = {}
-  blog = Blog.objects.all()
-  context["blogs"] = blog
+    context = {}
+    blog_list = Blog.objects.all()
+    
+    
+    paginator = Paginator(blog_list, 5)  
+    page = request.GET.get('page') 
+    
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+      
+        blogs = paginator.page(1)
+    except EmptyPage:
+     
+        blogs = paginator.page(paginator.num_pages)
 
-  return render(request, "pages/blog.html", context)
+    context["blogs"] = blogs
+    return render(request, "pages/blog.html", context)
 
 
 def blog_single_view(request: HttpRequest, slug: str) -> HttpResponse:
 
   context = {}
+  all_blogs=Blog.objects.all()
+  randomized_blogs = random.sample(list(all_blogs), 5)
+  
+  random_5_blogs = randomized_blogs[:5]
   single_blog = Blog.objects.filter(slug = slug).first()
+  blog_about = BlogAbout.objects.filter(post=single_blog).first()
+  blog_content = BlogContent.objects.filter(blog=single_blog)
+
+  if blog_about and blog_content:
+    context['blog_about'] = blog_about
+    context['blog_content'] = blog_content
+    
   context["blog"] = single_blog
+  
+  context["blogs"] = random_5_blogs
 
   return render(request, "pages/blogs/blog.html", context)
