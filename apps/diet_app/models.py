8c8923cc13a4f1053aa37a6c24ed7a63c845ad7b
CoderMungan <codermungan@gmail.com>
2024-02-07 13:51:37 +0300

    refactor: remove HeaderSlider Model

--- apps/diet_app/models.py
+++ apps/diet_app/models.py
@@ -3,22 +3,6 @@ from django.db import models
 from django.db import models
 from apps.diet_app.mixin import TimeBasedStampModel
 from ckeditor.fields import RichTextField
-from autoslug import AutoSlugField
-
-# Services Model ORM Start
-class HomeSlider(TimeBasedStampModel):
-  header = models.CharField(("Header"), max_length=50)
-  content = RichTextField(("slider içerik"))
-  photo = models.ImageField(("İçerik Resim"), upload_to="HomeCarousel")
-
-  class Meta:
-    verbose_name = 'Ana Sayfa Slider'
-    verbose_name_plural = 'Ana Sayfa Slider'
-
-  def __str__(self) -> str:
-      return self.header
-  
-# Services Model ORM End
 
 class TopContent(TimeBasedStampModel):
   content = RichTextField(("içerik"))
