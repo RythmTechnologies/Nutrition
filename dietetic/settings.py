c3f017b01ff5e6403d90ab149e88fc7012374dbb
mciray <melihciray@gmail.com>
2024-02-08 15:25:05 +0300

    staticfiles revize

--- dietetic/settings.py
+++ dietetic/settings.py
@@ -191,7 +191,6 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, "static/"),
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'basic',
