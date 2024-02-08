f40a139933e59ffb89fd2a91a43b42e9ad5d6489
mciray <melihciray@gmail.com>
2024-02-08 03:27:03 +0300

    vercel

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,7 @@ pip install --upgrade pip
 echo "requirements kuruluyor..."
 pip install -r requirements.txt
 
+echo "Statik dosyalar toplanıyor..."
+python manage.py collectstatic --noinput
+
 echo "Tamamlandı"
