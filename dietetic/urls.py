9a9e9c16cc12522b0e8aeac5af1a8324cae8a33d
mciray <melihciray@gmail.com>
2024-02-08 15:36:03 +0300

    qwe

--- dietetic/urls.py
+++ dietetic/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('iletisim/',include('apps.contact.urls')),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
