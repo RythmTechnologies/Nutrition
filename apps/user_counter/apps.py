4224c76052edc26be277e839e2cc8f19d5a3f507
mciray <melihciray@gmail.com>
2024-02-21 18:14:48 +0300

    feat:user visit deleted

--- apps/user_counter/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserCounterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.user_counter'
--- apps/user_counter/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from apps.diet_app.mixin import TimeBasedStampModel
-
-class Visit(TimeBasedStampModel):
-    ip_address = models.CharField(max_length=15,blank=True)  # IPv4 adresleri için
-    visit_count = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.ip_address} - {self.visit_count}"
